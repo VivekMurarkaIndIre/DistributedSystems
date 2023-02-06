542bd80b2ba3d2eec31a2ed82262022ef2d58149
Vivek Murarka <vivek.murarka@ucdconnect.ie>
2023-02-06 22:07:17 +0000

    Spring boot: Spring data JPA

---
+++ src/main/java/com/vivek/practice/dao/EmployeeDataJPARepository.java
@@ -0,0 +1,13 @@
+package com.vivek.practice.dao;
+
+import com.vivek.practice.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Here we are escaping CRUD operation we did using JPA earlier by now using spring data JPA
+ * It is observed that direct JDBC query are faster than JPA
+ * Spring Data JPA is almost as fast as JDBC query and faster than JPA for simple databases with minimal relations
+ * less boilerplate code is the biggest advantage
+ */
+public interface EmployeeDataJPARepository extends JpaRepository<Employee,Integer> {
+}
